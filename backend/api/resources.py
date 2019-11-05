71b63afc4658fe3442435c60c5e75850ff4148b7
Steven Huang <lingxiaohuang@0587404898.wireless.umich.net>
2019-11-05 13:41:16 -0500

    added more event handlers on the flask server

--- backend/api/resources.py
+++ backend/api/resources.py
@@ -3,10 +3,10 @@ import flask
 import backend
 
 
-@backend.app.route('/api/v1/class_description/<class_name>/', methods=["GET"])
+@backend.app.route('/api/v1/class_description/', methods=['GET', 'POST'])
 def get_class_description(class_name):
     """
-    Return the description of a class that's being asked by the students
+    Handle the description competency JSON payloads with B.L. 
     """
     context = {
         "student_name": 'Steven Huang',
@@ -15,3 +15,37 @@ def get_class_description(class_name):
         "description": 'an introduction class on data structure and algorithm'
     }
     return flask.jsonify(**context)
+
+
+@backend.app.route('/api/v1/class_recommendation/', methods=['GET', 'POST'])
+def get_class_recommendation():
+    """
+    Handle the recommendation competency JSON payloads with B.L. 
+    """
+    return 
+
+
+@backend.app.route('/api/v1/clean_bye/', methods=['GET', 'POST'])
+def get_clean_goodbye():
+    """
+    Clean the database with user specific information
+    """
+    return 
+
+
+@backend.app.route('/api/v1/clean_hello/', methods=['GET', 'POST'])
+def get_clean_hello():
+    """
+    Clean the database with user specific information
+    """
+    return
+
+
+@backend.app.route('/', methods=['GET'])
+def index():
+    """
+    Redirect any visit to the website to the REST 
+
+    TODO: direct to the react front end in the future 
+    """
+    return flask.redirect('/api/v1/class_description/')
