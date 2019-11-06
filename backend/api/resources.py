dae9860f46d9436ce2016f8b75a8ae49860501d5
Steven Huang <lingxiaohuang@Stevens-MacBook-Pro-4.local>
2019-11-05 19:54:29 -0500

    added to resourcespy

--- backend/api/resources.py
+++ backend/api/resources.py
@@ -3,21 +3,44 @@ import flask
 import backend
 
 
-@backend.app.route('/api/v1/class_description/', methods=['GET', 'POST'])
-def get_class_description(class_name):
+@backend.app.route('/api/v1/course_description/', methods=['GET', 'POST'])
+def get_class_description():
     """
     Handle the description competency JSON payloads with B.L. 
     """
     context = {
+        "response": "this is the response of a get request, you should not see this",
         "student_name": 'Steven Huang',
-        "course_name": class_name,
+        "course_name": 281,
         "url": flask.request.path,
         "description": 'an introduction class on data structure and algorithm'
     }
+
+    if flask.request.method == 'POST':
+        # handling the json payload and run them through simple business logic first
+        raw_slots = flask.request.json['response']['slots'] # slots is an array of slot dictionaries
+
+        slot_map = {}
+        for slot in raw_slots:
+            slot_map[slot['slot']] = slot['raw_value'][0]
+
+        print(slot_map)
+
+        class_name = slot_map['_COURSE_MAPPER_']
+
+        # modify the json payload to construct the response
+        response = flask.request.json
+        response['response']['slots'].append({
+                'slot': 'description',
+                'raw_value': ['eecs281 is an introduction class that covers data structure and algorithms']
+            })
+
+        return flask.jsonify(**response)
+
     return flask.jsonify(**context)
 
 
-@backend.app.route('/api/v1/class_recommendation/', methods=['GET', 'POST'])
+@backend.app.route('/api/v1/course_recommendation/', methods=['GET', 'POST'])
 def get_class_recommendation():
     """
     Handle the recommendation competency JSON payloads with B.L. 
@@ -48,4 +71,4 @@ def index():
 
     TODO: direct to the react front end in the future 
     """
-    return flask.redirect('/api/v1/class_description/')
+    return flask.redirect('/api/v1/course_description/')
