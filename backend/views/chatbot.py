153497a77e3400666ef1a7f9d06bda17cc419fd9
AJ DiDonato <ajdidonato@AJs-MacBook-Pro.local>
2019-11-17 17:34:07 -0500

    not working but messed with chatbot.py

--- backend/views/chatbot.py
+++ backend/views/chatbot.py
@@ -1,8 +1,34 @@
 import flask
 import backend
+import requests
 
 from flask import request, abort
 
 @backend.app.route('/chatbot', methods=['GET', 'POST'])
 def chatbot():
-    return flask.render_template("chatbot.html")
+
+    url1 = "https://3.133.94.198/v1/oauth/"
+    #url2 = "https://3.133.104.132/v1/query/"
+    #url3 = "http://3.133.104.132/v1/apps/applicationtoken/"
+
+    # Data when using the password grant type
+    payload =(
+        "institution=eecs498team4",
+        "&username=user6",
+        "&password=password6",
+        "&grant_type=password"
+    )
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    print("sending payload")
+    response = requests.post(url1, json=payload, headers=headers)
+    print("payload received")
+
+    print(response.json())
+
+    return flask.render_template("chatbot.html", response)
+
+
